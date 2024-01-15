2746d548251740e6c0715f1481428b95c846ebfa
dhfma1010 <dhfma1010@gmail.com>
2024-01-15 13:34:40 +0900

    v0.8

--- day02.py
+++ day02.py
@@ -1,24 +1,11 @@
-base_number = int(input('Input base number : '))
-exponent_number = int(input('Input exponent number : '))
-# print(f'밑은 {base_number}, 지수는 {exponent_number}, 결과 값은 {base_number**exponent_number}')
-# 1) f-string , {}: 값 바뀔 변수, 함수 리턴 값
-print(f'밑은 {base_number}, 지수는 {exponent_number}, 결과 값은 {pow(base_number,exponent_number)}')
+# first_number = int(input("First number : "))
+# second_number = int(input("Second number : "))
+#
+# quotient = first_number // second_number
+# remainder = first_number % second_number    --------> 연산이 함수보다 효율 더 좋음
+# print(f'몫은 {quotient} 나머지는 {remainder} 입니다.')
 
-# 2) format function , 3.6v 하고도 호환됨
-print('밑은 {0}, 지수는 {1}, 결과 값은 {2}'.format(base_number,exponent_number,pow(base_number,exponent_number)))
-print('밑은 {}, 지수는 {}, 결과 값은 {}'.format(base_number,exponent_number,pow(base_number,exponent_number)))
+first_number = int(input("First number : "))
+second_number = int(input("Second number : "))
 
-# 3) c like
-print('밑은 %d, 지수는 %d, 결과 값은 %d' % (base_number, exponent_number, pow(base_number,exponent_number)))
-
-
-# money = 5,000,000
-# print(money)
-# print(type(money)) # tuple
-# cash = 5_000_000
-# print(cash)
-# print(type(cash)) # int
-
-
-
-
+print(f'몫은 {divmod(first_number,second_number)[0]} 나머지는 {divmod(first_number,second_number)[1]} 입니다.')
