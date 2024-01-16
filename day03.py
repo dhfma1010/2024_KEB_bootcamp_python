30c2a86bdb229bd7aedeecb32db5dcff0ddd08a6
dhfma1010 <dhfma1010@gmail.com>
2024-01-16 11:39:48 +0900

    v1.9

--- day03.py
+++ day03.py
@@ -1,26 +1,6 @@
-
-course = "* KEB 2024#! KEB !bootcamp KEB...*!#"
-print(course.find('KEB'))  # 앞에서 부터 찾기
-print(course.rfind('KEB'))   # 뒤에서 부터 찾기
-print(course.index('KEB'))  # find랑 같은 기능
-print(course.rindex('KEB'))  # find랑 같은 기능
-print(course.find('Inha'))  # -1  --> if문 사용해서 해결
-print(course.index('Inha')) # ValueError: substring not found
-
-
-print(course)
-course = course.replace('KEB','Inha',2)
-print(course)
-
-print(course.strip())
-print(course.strip("!#.*"))  # 연속적인 것,맨 앞쪽 지워짐, 중간중간 안지워짐
-
-
-
-
-# print(course)
-# print(course.replace('KEB','Inha'))
-# print(course)
-# course = course.replace('KEB','Inha')
-# print(course) # 재할당 됨
-
+subjects = "python c++ database linux"
+subject = input("수강신청 과목 입력 : ")
+if subjects.find(subject) != -1:
+    print(f"해당 과목은 존재합니다. 위치는 {subjects.find(subject)}번 인덱스")
+else:
+    print('해당 과목이 존재하지 않습니다.')
