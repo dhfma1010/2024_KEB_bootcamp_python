c455f1514a20c30a25e023b262b5eeaa1b1e71fc
dhfma1010 <dhfma1010@gmail.com>
2024-01-16 14:36:52 +0900

    v2.5 prime number day03

--- day03.py
+++ day03.py
@@ -1,16 +1,17 @@
 # prime number
 number = int(input("Input number : "))
+is_prime = True # int -> bool
 
-cnt = 0
 i = 2
 while i < number:
     if number % i == 0:
-        cnt = cnt + 1
+        is_prime = False # remove +
         break  # break 없으면 계속 돌아감
     print(i, end=' ')   #몇 번 도는지 확인용
     i = i + 1
 
-if cnt == 0:
+# if cnt ==0:
+if is_prime: # remove ==
     print(f'{number} is prime number')
 else:
     print(f'{number} is NOT prime number!')
