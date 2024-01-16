dc8ae2325bcab9e5338687f4502790bcbba51c46
dhfma1010 <dhfma1010@gmail.com>
2024-01-16 16:34:04 +0900

    v3.3 Assignment02_1 day03

--- day03.py
+++ day03.py
@@ -1,59 +1,8 @@
-# Assignment (v0.2 loop) 3),4) 추가
+#1
 
-while True:
+print(list(range(3,-1,-1)))
 
-    menu = input("1) Fahrenheit -> Celsius 2) Celsius -> Fahrenheit 3) prime number or not 4) n1~n2 prime numbers 5) Quit program : ")
+list = [3,2,1,0]
+for i in list:
+    print(i)
 
-    if menu == '1':
-        fahrenheit = float(input('Input Fahrenheit : '))
-        print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit - 32.0) * 5.0/9.0):.4f}C')
-
-    elif menu == '2':
-        celsius = float(input('Input Celsius : '))
-        print(f'Celsius : {celsius}C, Fahrenheit : {((celsius * 9.0/5.0) + 32.0):.4f}F')
-
-    elif menu == '3':
-        number = int(input("Input number : "))
-        is_prime = True  # int -> bool
-
-        if number < 2:
-            print(f'{number} is NOT prime number!')
-        else:
-            i = 2
-            while i < number:
-                if number % i == 0:
-                    is_prime = False  # remove +
-                    break  # break 없으면 계속 돌아감
-                i = i + 1
-
-            # if cnt ==0:
-            if is_prime:  # remove ==
-                print(f'{number} is prime number')
-            else:
-                print(f'{number} is NOT prime number!')
-
-    elif menu == '4':
-        numbers = input("Input first second number : ").split()
-        n1 = int(numbers[0])
-        n2 = int(numbers[1])
-
-        if n1 > n2:
-            n1, n2 = n2, n1
-
-        for number in range(n1, n2 + 1):
-            is_prime = True
-
-            if number < 2:
-                pass
-            else:
-                for i in range(2, number):
-                    if number % i == 0:
-                        is_prime = False  # remove +
-                        break  # break 없으면 계속 돌아감
-                if is_prime: print(number, end=' ')
-        print()
-
-
-    elif menu == '5':
-        print('Terminate Program.')
-        break
