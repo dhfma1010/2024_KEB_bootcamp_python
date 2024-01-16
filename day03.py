64cc02852bb00498813930739a2bac35e6dd3dc0
dhfma1010 <dhfma1010@gmail.com>
2024-01-16 10:51:20 +0900

    v1.7

--- day03.py
+++ day03.py
@@ -24,7 +24,18 @@ name = 'aabbccdd'
 name.replace('cc','ee')
 print(name)
 
-course = "2024 KEB bootcamp"
-print(course)
-list_course = course.split()
-print(list_course)
+# course = "2024 KEB bootcamp"
+# print(course)
+# # list_course = course.split()
+# list_course = course.split('B')
+# print(list_course)
+
+
+numbers = input("FirstNumber SecondNumber : ").split()
+# print(numbers[0] + numbers[1])   # concatenation
+print(int(numbers[0]) + int(numbers[1]))   # arithmetic operation
+
+
+subjects = ["python", "c++", "database"]
+subjects_string = " / ".join(subjects)
+print(subjects_string)
