24e8f5ed0206238304bcb6e6b071fa541c9ed5f2
dhfma1010 <dhfma1010@gmail.com>
2024-01-16 15:42:20 +0900

    v3.1 prime number day03

--- day03.py
+++ day03.py
@@ -4,11 +4,16 @@ numbers = input("Input first second number : ").split()
 n1 = int(numbers[0])
 n2 = int(numbers[1])
 
+if n1 > n2:
+    n1, n2 = n2, n1
+
+# is_prime = True ## 여기 있으면 소수 전체 출력 안됨!!
 for number in range(n1, n2+1):
-    is_prime = True
+    is_prime = True ## 여기 있어야 함!!!!!! is_prime = True로 계속 초기화 시켜야 함.
 
     if number < 2:
-        pass # 자리 차지하는데 그냥 지나감
+        # pass # 자리 차지하는데 그냥 지나감
+        continue
     else:
         for i in range(2, number):
             if number % i == 0:
