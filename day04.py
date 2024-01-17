2963d894d22cc0ffa3a29ed5512b278c48311bb0
dhfma1010 <dhfma1010@gmail.com>
2024-01-17 17:49:25 +0900

    v5.2 assignment01 day04

--- day04.py
+++ day04.py
@@ -1,22 +1,5 @@
-# Assignment 8.6~8.9
+# Assignment
 
-life = dict(animals=dict(cats="Henri", octopi="Grumpy", emus="Lucy"),
-            plants=dict(),
-            other=dict())
-life2 = {'animals':{'cats':'Henri','octopi':'Grumpy','emus':'Lucy'},
-         'plants':{},
-         'other':{}}
-print(life2)
-print(life)
 
-# 8.7
-
-print(list(life.keys()))
-
-# 8.8
-
-print(list(life['animals'].keys()))
-
-# 8.9
-
-print(life['animals']['cats'])
+squares = {i: i*i for i in range(10)}
+print(squares)
