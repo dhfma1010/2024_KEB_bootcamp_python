7c893494e2cd7681069194c0beeddd7f90bf8281
dhfma1010 <dhfma1010@gmail.com>
2024-01-17 16:53:05 +0900

    v5.0 assignment01 day04

--- day04.py
+++ day04.py
@@ -4,17 +4,13 @@
 e2f = dict(dog='chien', cat='chat', walrus='morse')
 print(e2f)
 
-# 8.4
+# 8.5
 
-e2f_keys = list(e2f.keys())
-e2f_values = list(e2f.values())
+e2f_keys_tuple = tuple(e2f.keys())
+e2f_keys_list = list(e2f.keys())
+print(f'English words : {e2f_keys_tuple}')
+print(f'English words : {e2f_keys_list}')
 
-reverse_e2f = {value: key for value,key in zip(e2f_values,e2f_keys)}
-print(reverse_e2f.get('chien'))
-
-# 추가로
-
-French_word = input('프랑스 단어 입력 chien / chat / morse : ')
-print(reverse_e2f.get(French_word))
-
-# 번역 프로그램 만들어 볼 수 있음
+print('English words :', end=' ')
+for eng_word in e2f_keys_tuple:
+    print(eng_word, end=' ')
