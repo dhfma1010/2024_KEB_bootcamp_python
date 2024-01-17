ef985cfdba7ac6463c8948e58332dbb9db54c761
dhfma1010 <dhfma1010@gmail.com>
2024-01-17 14:09:40 +0900

    v4.3 comprehention day04

--- day04.py
+++ day04.py
@@ -1,13 +1,23 @@
-# subjects = ["a", "b", "c"] # b,c,d 값 바뀌지 x(immutable한 값 )
-import copy
+# list 컴프리헨션
 
-subjects = ["a",["b","c"], "d"] #  vs list (mutable한 값이라 복제한 것도 다 변화)
-a = subjects
-b = subjects.copy()
-c = list(subjects)
-d = subjects[:]
-e = copy.deepcopy(a)
-print(subjects, a, b, c, d, e)
-subjects[1][1]= "x"
-print(subjects, a, b, c, d, e)
+# 1)
+# squares = list()
+# squares.append(1*1)
+# squares.append(2*2)
+# squares.append(3*3)
+# squares.append(4*4)
+# squares.append(5*5)
+# print(squares)
 
+# 2)
+# squares = list()
+# for i in range(1,6):
+#     squares.append(i*i)
+# print(squares)
+
+# 3) list comprehension 축약표현  -------- for문 여러개 일때는 가독성 문제로 잘 안씀
+# squares = [i*i for i in range(1,6,1)]
+# print(squares)
+
+even_squares = [i*i for i in range(1, 6, 1) if i % 2 == 0]
+print(even_squares)  # 리스트 초기화 할 때 유용
