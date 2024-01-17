bd1ef6afd0b67004626a169d45a67937645a75d4
dhfma1010 <dhfma1010@gmail.com>
2024-01-17 16:47:43 +0900

    v4.9 assignment01 day04

--- day04.py
+++ day04.py
@@ -4,27 +4,17 @@
 e2f = dict(dog='chien', cat='chat', walrus='morse')
 print(e2f)
 
-e2f_2 = {'dog':'chien', 'cat':'chat', 'walrus':'morse'}
-print(e2f_2)
+# 8.4
 
-# 8.2 값 얻기
+e2f_keys = list(e2f.keys())
+e2f_values = list(e2f.values())
 
-print(e2f['walrus'])
-print(e2f.get('walrus'))
+reverse_e2f = {value: key for value,key in zip(e2f_values,e2f_keys)}
+print(reverse_e2f.get('chien'))
 
-# 8.3 f2e
+# 추가로
 
-print(e2f.keys()) # e2f.keys()는 dict_keys class(타입)
-print(e2f.values())
+French_word = input('프랑스 단어 입력 chien / chat / morse : ')
+print(reverse_e2f.get(French_word))
 
-f2e_values = list(e2f.keys()) # 리스트로 변환
-print(f2e_values)
-f2e_keys = list(e2f.values())
-print(f2e_keys)
-
-f2e = dict(zip(f2e_keys,f2e_values)) # zip사용해서 dict만듦
-f2e_2 = {key: value for key, value in zip(f2e_keys,f2e_values)}  # dictionary comprehention
-e2f_3 = dict(zip(f2e_values,f2e_keys))
-print(f2e)
-print(f2e_2)
-print(e2f_3)
+# 번역 프로그램 만들어 볼 수 있음
