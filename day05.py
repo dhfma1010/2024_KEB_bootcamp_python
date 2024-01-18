b569326e091ebffe4ac343ca93eda67988e9a6e2
dhfma1010 <dhfma1010@gmail.com>
2024-01-18 18:34:39 +0900

    v6.9 v1.4review day05

--- day05.py
+++ day05.py
@@ -1,22 +1,20 @@
-# assignment 예외정의
+# Fahrenheit - Celsius 보완
+# 1) while 구문 가장 바깥에 2) else 대신 elif menu==3 ; 3눌러야 quit, 다른 입력값 이면 계속 반복
 
-class OopsException(Exception):
-    pass
-
-words = []
 while True:
-    word = input("Input word: ")
-    words.append(word)
 
-    if 'oops' in words:
-        raise OopsException(word)
+    menu = input("1) Fahrenheit -> Celsius 2) Celsius -> Fahrenheit 3) Quit program : ")
 
-    print(words)
+    if menu == '1':
+        fahrenheit = float(input('Input Fahrenheit : '))
+        print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit - 32.0) * 5.0/9.0):.4f}C')
+        # menu = input("1) Fahrenheit -> Celsius 2) Celsius -> Fahrenheit 3) Quit program : ") # 이부분 중복
 
-try:
-    raise OopsException('Caught an oops')
-except OopsException as exc:
-    print(exc)
+    elif menu == '2':
+        celsius = float(input('Input Celsius : '))
+        print(f'Celsius : {celsius}C, Fahrenheit : {((celsius * 9.0/5.0) + 32.0):.4f}F')
+        # menu = input("1) Fahrenheit -> Celsius 2) Celsius -> Fahrenheit 3) Quit program : ") # 이부분 중복 --> while문 제일 바깥에 써야
 
-
-
+    elif menu == '3':
+        print('Terminate Program.')
+        break
