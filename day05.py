034a7305f261177dee07708c885cb6bd392002bb
dhfma1010 <dhfma1010@gmail.com>
2024-01-18 16:43:02 +0900

    v6.6 assignment day05

--- day05.py
+++ day05.py
@@ -1,12 +1,11 @@
-# assignment 9.1
+# assignment 9.2 generator
 
-def good() ->list:
-    print(['Harry', 'Ron', 'Hermione'])
-
-good()
-
-def good_harry():
-    return ['Harry', 'Ron', 'Hermione']
+def get_odds():
+    n = [i for i in range(10) if i % 2 == 1]
+    yield n
 
 
-print(good_harry())
+print(get_odds(), type(get_odds()))
+
+for number in get_odds():
+    print(number[2])
