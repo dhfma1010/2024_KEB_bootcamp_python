9c38163cb25f4627b887753c18d156c1e1c31031
dhfma1010 <dhfma1010@gmail.com>
2024-01-18 13:36:10 +0900

    v5.9 def day05

--- day05.py
+++ day05.py
@@ -1,10 +1,20 @@
 # 매개변수로 함수 들어갈 수 있음
 
-def squares(n):
-    return n * n
+def squares(*n) -> list:
+    """
+    넘겨 받은 수치 데이터들의 거듭제곱 값을 리스트에 담아서 리턴
+    :param n: tuple
+    :return: list
+    """
+    return [i*i for i in n] # 튜플로 리턴하려며??
 
-def run_function(f, number):
-    return f(number)
+    # return n * n
 
-print(squares(7))
-print(run_function(squares,9))
+def run_function(f, *number) -> list:
+     return f(*number)
+
+print(squares(7,5,2))
+
+print(run_function(squares,9,10))
+
+
