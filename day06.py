3fe9cbfb7d7a3f0c20036b0a0d4efa47afc43ded
dhfma1010 <dhfma1010@gmail.com>
2024-01-19 13:44:51 +0900

    v8.3 class day06

--- day06.py
+++ day06.py
@@ -1,18 +1,36 @@
-# class Pokemon():
+# # class Pokemon():
+#
+# # 객체에 속성 할당
+#
+# class Pokemon:
+#     pass
+#
+# pikachu = Pokemon()
+# squirtle = Pokemon()
+# pikachu.name = "피카츄"
+# pikachu.nemesis = squirtle
+# print(pikachu.name)
+# # print(pikachu.nemesis.name) # 오류!
+#
+# # squirtle.name = "꼬부기"
+# pikachu.nemesis.name = "꼬부기"
+# print(pikachu.nemesis.name)  # 할당 후 오류 안남!
+# print(squirtle.name)
 
-# 객체에 속성 할당
+
+
 
 class Pokemon:
-    pass
+    def __init__(self, name):  # 여기 name은 클래스의 속성 아님! 그냥 단순 출력
+        self.name = name # 이 코드가 클래스 속성 넣는 것
+        print(f"{name} 포켓몬스터 생성")
 
-pikachu = Pokemon()
-squirtle = Pokemon()
-pikachu.name = "피카츄"
-pikachu.nemesis = squirtle
+    def attack(self, target):
+        print(f"{self.name}이(가) {target.name}을(를) 공격!")
+
+charizard = Pokemon("리자몽")
+pikachu = Pokemon("피카츄")
+squirtle = Pokemon("꼬부기")
+charizard.attack(squirtle)
 print(pikachu.name)
-# print(pikachu.nemesis.name) # 오류!
-
-# squirtle.name = "꼬부기"
-pikachu.nemesis.name = "꼬부기"
-print(pikachu.nemesis.name)  # 할당 후 오류 안남!
 print(squirtle.name)
