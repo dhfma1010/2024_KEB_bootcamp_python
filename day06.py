b757383590c9560537837e6c3fdc1d99282169f4
dhfma1010 <dhfma1010@gmail.com>
2024-01-19 11:12:35 +0900

    v7.7 error day06

--- day06.py
+++ day06.py
@@ -1,5 +1,8 @@
 import random
 
+class OoopsException(Exception): # 괄호 안 상속
+    pass
+
 numbers = []
 for i in range(5):
     numbers.append(random.randint(1, 100))
@@ -14,7 +17,9 @@ print(numbers)
 try:
     pick = int(input(f'Input index (0 ~ {len(numbers)-1}) : '))
     print(numbers[pick])
-    print(5/0)
+    print(5/2)
+    raise OoopsException("Oops")  # exception!
+
 
 # except: # 모든 걸 잡음. 에러 아닌 것도!
 
@@ -25,5 +30,11 @@ except ValueError as err:  # input은 받는데 int로 넘어갈 때 에러
 except ZeroDivisionError as err:
     print(f"The denominator cannot be 0. \n{err}")
 
+#except OoopsException as err:
+#    print(f'Oops Oops {err}')
+
 except Exception as err : # 가장 마지막에 와야함! !
     print(f"Error occurs : {err}")
+
+else:
+    print(f'Program terminate')
