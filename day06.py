1cdbb347ed6e6157ee3e4629157fbcbc95101dfa
dhfma1010 <dhfma1010@gmail.com>
2024-01-19 15:16:33 +0900

    v8.7 class day06

--- day06.py
+++ day06.py
@@ -8,6 +8,8 @@ class SwimmingMixin:
 class Pokemon:
     def __init__(self, name):
         self.name = name
+    def attack(self):
+        print("공격")
 
 class Charizard(Pokemon, FlyingMixin):
     pass
@@ -19,3 +21,7 @@ g1 = Gyarados("갸라도스")
 c1 = Charizard("리자몽")
 print(c1.fly())
 print(g1.swim())
+c1.attack()
+Charizard.attack(c1) # 원래 객체명.메소드() self.attack() // class.attack(self) 클래스 이름 올 경우 객체명(g1,c1등) 괄호 안에 넣어야 함
+# Charizard.attack() # 오류남
+
