7001c0e79a8debf0a4a1228fd6c86a5281359e21
dhfma1010 <dhfma1010@gmail.com>
2024-01-19 09:09:56 +0900

    v7.0 assingment_sol day06

---
+++ day06.py
@@ -0,0 +1,12 @@
+# assignment
+
+def good()->list:
+    """
+    chapter 9 things to do 9.1
+    :return: list
+    """
+
+    harry_porter = input().split()
+    return harry_porter
+
+print(good())
