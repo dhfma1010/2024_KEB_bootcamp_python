387df3bfcfea3fc42e1525d51b3e581f2d95efcb
dhfma1010 <dhfma1010@gmail.com>
2024-01-19 15:17:45 +0900

    v8.8 class day06

--- day06.py
+++ day06.py
@@ -19,9 +19,15 @@ class Gyarados(Pokemon, SwimmingMixin):
 
 g1 = Gyarados("갸라도스")
 c1 = Charizard("리자몽")
-print(c1.fly())
-print(g1.swim())
-c1.attack()
-Charizard.attack(c1) # 원래 객체명.메소드() self.attack() // class.attack(self) 클래스 이름 올 경우 객체명(g1,c1등) 괄호 안에 넣어야 함
-# Charizard.attack() # 오류남
+
+print(g1.name)
+g1.name = "잉어킹" # direct로 변경
+print(g1.name)
+
+
+# print(c1.fly())
+# print(g1.swim())
+# c1.attack()
+# Charizard.attack(c1) # 원래 객체명.메소드() self.attack() // class.attack(self) 클래스 이름 올 경우 객체명(g1,c1등) 괄호 안에 넣어야 함
+# # Charizard.attack() # 오류남
 
