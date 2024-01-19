06061cad8799d7348ea9e55878c87903e79660f3
dhfma1010 <dhfma1010@gmail.com>
2024-01-19 11:24:20 +0900

    v7.9 day06

--- day06.py
+++ day06.py
@@ -1,13 +1,20 @@
-def desc():
+def desc(f):
     def wrapper(): # 호출하지 않으면 실행되지 않음
         print("study")
-    print("a")
+        f() # 외부에서 선언된 매개변수와 같은 매개변수
     return wrapper
-
-desc() # wrapper실행 안됨
 
 
 def something():  # 건드리지 않고 확장 -> 데코레이터
     print("do something")
 
 something()
+
+s = desc(something)  # wrapper(closure)가 f(외부 함수의 매개 변수) 기억하고 있음!
+s()
+
+
+@desc
+def something():
+    print("do something")
+
