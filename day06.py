c72ea6ba5248d529322e404fcd5f539a603e75b1
dhfma1010 <dhfma1010@gmail.com>
2024-01-19 09:16:19 +0900

    v7.1 assingment_sol 9.2 day06

--- day06.py
+++ day06.py
@@ -1,12 +1,27 @@
 # assignment
 
-def good()->list:
+def get_odds(n) -> int:
     """
-    chapter 9 things to do 9.1
-    :return: list
+    1부터 n까지 홀수를 발생시키는 제너레이터
+    :param n: int
+    :return: int
     """
+    for i in range(1,n+1,2):
+        yield i # i값 계속 보내줌 , 이전 상태 사라짐
 
-    harry_porter = input().split()
-    return harry_porter
+cnt = 0
+odds = get_odds(9)
 
-print(good())
+for odd in odds:
+    cnt = cnt + 1
+    if cnt == 3:
+        print(f'Third number is {odd}')
+        break
+
+
+# 제너레이트 이므로 또 반복 안되고 사라짐
+for odd in odds:
+    cnt = cnt + 1
+    if cnt == 3:
+        print(f'Third number is {odd}')
+        break
