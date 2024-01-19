bcc05f21e305df7b9a48cf55ce843e5d63bec2ce
dhfma1010 <dhfma1010@gmail.com>
2024-01-19 11:40:52 +0900

    v8.0 class day06

--- day06.py
+++ day06.py
@@ -1,20 +1,9 @@
-def desc(f):
-    def wrapper(): # 호출하지 않으면 실행되지 않음
-        print("study")
-        f() # 외부에서 선언된 매개변수와 같은 매개변수
-    return wrapper
+# class Pokemon():
+class Pokemon:
+    def __init__(self, name): # 생성자, 각 개체마다 딱 1번 돌아가는 코드
+        print(f"{name} 포켓몬스터 생성")
 
 
-def something():  # 건드리지 않고 확장 -> 데코레이터
-    print("do something")
-
-something()
-
-s = desc(something)  # wrapper(closure)가 f(외부 함수의 매개 변수) 기억하고 있음!
-s()
-
-
-@desc
-def something():
-    print("do something")
+pikachu = Pokemon("피카츄")
+squirtle = Pokemon("꼬부기")
 
