ea3fd9e095f0cdaae2d123307b6ba949f2b2f220
dhfma1010 <dhfma1010@gmail.com>
2024-01-22 11:32:21 +0900

    v9.9 pokemon day07

--- day07.py
+++ day07.py
@@ -23,71 +23,13 @@ class JetPack(FlyingBehavior):
 class SwimmingBehavior:
     def swim(self):
         return f"{self.__name}이(가) 수영을 합니다."
-
-class Pokemon:
+class Pikachu:
     def __init__(self, name, hp, fly):
-        self.__name = name
+        self.name = name
         self.hp = hp
-        self.fly_beh = fly  ## aggregation (has-a)
-
-    def set_fly_beh(self,fly):    ## setter
         self.fly_beh = fly
-
-
-
-    def attack(self):
-        print("공격~")
-
-    @property
-    def name(self):
-        return self.__name
-
-    @name.setter
-    def name(self, new_name):
-        self.__name = new_name
-
-    #name = property(get_name, set_name)
-
-    def __str__(self):  # 매직 메서드
-        return self.__name + "입니다."
-
-    # def __add__(self, other):
-    #     list1=self.name[0:2]
-    #     list2=other.name[-1]
-    #     name=list1+list2
-    #     return str(name)
-
-    def __add__(self, target):
-        # return self.name + "와 " + target.__name + "입니다."
-        return f"두 포켓몬 체력 합은 {self.hp + target.hp}"    ##### 체력 늘어나고 줄어들게
-
-
-class Charizard(Pokemon):
-    pass
-
-class Gyarados(Pokemon):
-    pass
-
-class Pikachu(Pokemon):
-
-
 
 
 nofly = NoFly()
 p1 = Pikachu("피카츄", 35, nofly)
-
-wings = FlyWithWings() # 다른 클래스 객체 만들어서 리자몽에게 전달
-c1 = Charizard("리자몽", 120, wings) # aggregation
-
-print(c1.fly_beh.fly()) # fly객체의 fly사용
-print(p1.fly_beh.fly())
-
-print(p1)  # <__main__.Gyarados object at 0x000001A6A078E0C0>
-print(c1)
-
-# print(p1+c1) # 정수나 문자 연산 + 아니므로 오류 / 새로 매직메서드 필요
-
-p1.set_fly_beh(JetPack()) # 아이템 장착으로 변화
-print(p1.fly_beh.fly())
-
-
+print(p1.fly_beh.fly()) # aggregation
