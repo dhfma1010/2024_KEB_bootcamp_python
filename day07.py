8eccf69f27a524638562eb8f41f51ee2b2f759b6
dhfma1010 <dhfma1010@gmail.com>
2024-01-22 13:30:00 +0900

    v10.6  module day07

--- day07.py
+++ day07.py
@@ -1,38 +1,41 @@
 # module
-# 1) import mymath
-# 2) from mymath import * # 짧지만 다른 모듈의 같은 이름 가진 함수와 충돌 위험 (별로 안 씀)
+
 
 import mymath as mm # 3) 줄여 쓰는 세번째 방법 (자주 씀)
 
-while True:
-    menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3) Prime1   4) Prime2   5) Quit program : ")
+# print(globals())
 
-    if menu == '1':
-        fahrenheit = float(input('Input Fahrenheit : '))
-        print(f'Fahrenheit : {fahrenheit}F, Celsius : {mm.fahrenheit_to_celsius(fahrenheit):.4f}C')
-    elif menu == '2':
-        celsius = float(input('Input Celsius : '))
-        print(f'Celsius : {celsius}C, Fahrenheit : {mm.celsius_to_fahrenheit(celsius):.4f}F')
-    elif menu == '3':
-        number = int(input("Input number : "))
-        if mm.isprime(number):
-            print(f'{number} is prime number')
+if __name__ == "__main__": # 실행시키는 파일 main인지 아닌지 확인
+
+    while True:
+        menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3) Prime1   4) Prime2   5) Quit program : ")
+
+        if menu == '1':
+            fahrenheit = float(input('Input Fahrenheit : '))
+            print(f'Fahrenheit : {fahrenheit}F, Celsius : {mm.fahrenheit_to_celsius(fahrenheit):.4f}C')
+        elif menu == '2':
+            celsius = float(input('Input Celsius : '))
+            print(f'Celsius : {celsius}C, Fahrenheit : {mm.celsius_to_fahrenheit(celsius):.4f}F')
+        elif menu == '3':
+            number = int(input("Input number : "))
+            if mm.isprime(number):
+                print(f'{number} is prime number')
+            else:
+                print(f'{number} is NOT prime number!')
+        elif menu == '4':
+            numbers = input("Input first second number : ").split()
+            n1 = int(numbers[0])
+            n2 = int(numbers[1])
+
+            if n1 > n2:
+                n1, n2 = n2, n1
+
+            for number in range(n1, n2 + 1):
+                if isprime(number):
+                    print(number, end=' ')
+            print()
+        elif menu == '5':
+            print('Terminate Program.')
+            break
         else:
-            print(f'{number} is NOT prime number!')
-    elif menu == '4':
-        numbers = input("Input first second number : ").split()
-        n1 = int(numbers[0])
-        n2 = int(numbers[1])
-
-        if n1 > n2:
-            n1, n2 = n2, n1
-
-        for number in range(n1, n2 + 1):
-            if isprime(number):
-                print(number, end=' ')
-        print()
-    elif menu == '5':
-        print('Terminate Program.')
-        break
-    else:
-        print('Invalid Menu!')
+            print('Invalid Menu!')
